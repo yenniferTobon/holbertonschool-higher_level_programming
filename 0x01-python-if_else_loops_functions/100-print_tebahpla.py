4793282fda791cf541a2c5e97f21242b4e0911ed
yenniferTobon <ymtobony@unal.edu.co>
2019-09-11 21:49:36 +0000

    100-print_tebahpla.py prints the ASCII alphabet, in reverse order, alternating

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for n in range(122, 96, -1):
-    if (n % 2 == 0):
+    if n % 2 == 0:
         pass
     else:
         n = n - 32
-    print("{}". format(chr(n)), end="")
+    print("{}".format(chr(n)), end="")
