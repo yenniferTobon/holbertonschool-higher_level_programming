0f6f88baa687715f178215f89f27ab41e9cc0f53
yenniferTobon <ymtobony@unal.edu.co>
2019-09-12 19:55:00 +0000

    1-calculation.py does some Maths, and prints the result.

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from add_0 import add
+from calculator_1 import *
+
+if __name__ == "__main__":
+    import sys
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
