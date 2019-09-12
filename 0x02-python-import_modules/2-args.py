0b64a1e4c5949d24df02f9e2661a2d24eac8e8b6
yenniferTobon <ymtobony@unal.edu.co>
2019-09-12 21:12:06 +0000

    2-args.py prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    from calculator_1 import sub
-    n = 0
     argvs = sys.argv
-    countArgv = len(argvs)
-    argvReal = sub(countArgv, 1)
+    argvReal = len(argvs) - 1
     if argvReal == 0:
         print("{} arguments.".format(argvReal))
     elif argvReal == 1:
