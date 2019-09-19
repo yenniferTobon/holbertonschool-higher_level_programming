d41dbe85d39b6b14a80f6a8d9c9f36059f997bd4
yenniferTobon <ymtobony@unal.edu.co>
2019-09-19 20:17:24 +0000

    0-main.py exercise 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[[]]):
-    lenMatrix = len(matrix)
     new_matrix = []
-    for i in range(lenMatrix):
-        new_row = []
-        for j in range(lenMatrix):
-            new_row += [matrix[i][j] * matrix[i][j]]
-        new_matrix += [new_row]
+    for row in matrix:
+        new_matrix += [list(map(lambda x: x * x, row))]
     return new_matrix
-    # for row in matrix:
-    #     new_matrix = map(lambda x: x * x, l)
