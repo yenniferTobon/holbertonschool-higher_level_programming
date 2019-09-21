57cd671eb810f3549cfe91b068045c7db8c7d392
yenniferTobon <ymtobony@unal.edu.co>
2019-09-21 03:57:27 +0000

    12-roman_to_int.py - function def roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == 'None':
-        return 0
+    if type(roman_string) != str or roman_string == 'None' or roman_string.isdigit():
+        return 0 
     new_list = []
     suma = 0
     diccio = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-
-    for idx in range(len(roman_string)):
-        for elem in diccio:
-            if roman_string[idx] == elem:
-                new_list.append(diccio[elem])
-    if len(new_list) != len(roman_string):
-        return (0)
-
     for k in range(len(roman_string)):
         new_list.append(diccio.get(roman_string[k]))
     for x in range(len(new_list)):
-        if x  == len(new_list) - 1 or new_list[x] >= new_list[x + 1]:
+        if x  == len(new_list) - 1:
+            return suma + new_list[x]
+        if new_list[x] >= new_list[x + 1]:
             suma += new_list[x]
         else:
-            suma -= new_list[x]
-    return (suma)
+            suma -= new_list[x]	
