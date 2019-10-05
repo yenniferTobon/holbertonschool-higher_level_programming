6de7fbc541b351a3de593b0fa044e2c3ada30361
yenniferTobon <ymtobony@unal.edu.co>
2019-10-05 21:26:16 +0000

    0-add_integer.py function that adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,10 +6,9 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
- 
+
     if type(a) is float:
         a = int(a)
     if type(b) == float:
         b = int(b)
     return a + b
-
