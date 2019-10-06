3eb89ff72f28d4b00bb19f5ccd25013149d18d6f
yenniferTobon <ymtobony@unal.edu.co>
2019-10-06 17:35:32 +0000

    5-text_indentation.py Write a function that prints a text with 2 new lines after each of these characters: ., ? and :

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 def text_indentation(text):
+    """
+    This module cuts line with
+    '., ?, ;' delimeters
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
     if text is not None:
         for i in range(len(text)):
             if text[i] is " " and text[i + 1] is " ":
                 continue
-            if (text[i] is " " and (text[i - 1] is "." or text[i - 1] is "?" or text[i - 1] is ":" or text[i - 1] is " ")):
+            if text[i] is " " and (text[i - 1] is "." or text[i - 1] is "?"):
                 continue
-            print("{}".format(text[i]), end="") 
+            if text[i] is " " and (text[i - 1] is ":" or text[i - 1] is " "):
+                continue
+            print("{}".format(text[i]), end="")
             if text[i] is "." or text[i] is "?" or text[i] is ":":
                 print()
                 print()
-        print()
