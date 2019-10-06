13017f24e1444bb7f6fa695594b2c1784133a498
yenniferTobon <ymtobony@unal.edu.co>
2019-10-06 18:34:06 +0000

    4-print_square.py function that prints a square with the character #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_square(size):
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if type(size) != int:
+    if type(size) is not int:
         raise TypeError("size must be an integer")
-    if type(size) == float or size < 0.0:
-        raise ValueError("size must be an integer")
     for row in range(size):
         for column in range(size):
             print("#", end="")
