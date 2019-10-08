6f187a6396027fe0598abba04f65713c7e371d98
yenniferTobon <ymtobony@unal.edu.co>
2019-10-08 02:02:51 +0000

    5-rectangle.py Print the message Bye rectangle...

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        rectangulo = ""
+        for altura in range(self.height):
+            i = 0
+            for base in range(self.width):
+                rectangulo += "#"
+                i = i + 1
+                if i == self.width and altura is not self.height - 1:
+                    rectangulo += "\n"
+        return rectangulo
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        print("Bye rectangle...")
