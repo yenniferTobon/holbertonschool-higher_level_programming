20184ef40ea2a84da5b44d0a644217352ecde343
yenniferTobon <ymtobony@unal.edu.co>
2019-10-16 19:41:39 +0000

    1-number_of_lines.py function that returns the number of lines of a text file

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for read file.
+"""
+
+
+def number_of_lines(filename=""):
+    """
+    function that returns the number of lines of a text file
+    """
+    numLine = 0
+    with open("my_file_0.txt", encoding="UTF-8") as f:
+        for line in f:
+            numLine = numLine + 1
+        return numLine
