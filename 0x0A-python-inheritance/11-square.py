2dc8f4329bb38ad0cd6c095b8b2536d857fc82f2
yenniferTobon <ymtobony@unal.edu.co>
2019-10-16 03:17:11 +0000

    11-square.py Write a class Square that inherits from Rectangle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+This module contains the classes:
+-BaseGeometry.
+-Rectangle.
+"""
 Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     def __init__(self, size):
@@ -9,7 +15,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-    
+
     def __str__(self):
         """
         Method called when using print()
