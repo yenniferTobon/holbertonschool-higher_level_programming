c9d8477fbe477934671b65eb13a349e2193d1b8c
yenniferTobon <ymtobony@unal.edu.co>
2019-10-16 02:44:11 +0000

    9-rectangle.py width and height must be private. No getter or setter, must be positive integers

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+This module contains the classes:
+-BaseGeometry.
+-Rectangle.
+"""
+
+class Rectangle(BaseGeometry):
+    """
+    Class that defines basic geometries.
+    """
+    def __init__(self, width, height):
+        """
+        Initializer for the rectangle class.
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """
+        Definition of the area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+       """
+       Method called when using print()
+       """
+       return "{} {}/{}".format("[Rectangle]", self.__width, self.__height)
