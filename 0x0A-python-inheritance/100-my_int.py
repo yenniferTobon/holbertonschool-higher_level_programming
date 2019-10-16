91688936fd8d03b4d2719e206a0358e02710cb76
yenniferTobon <ymtobony@unal.edu.co>
2019-10-16 02:46:40 +0000

    100-my_int.py Write a class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains the class MyInt.
+"""
+
+class MyInt(int):
+    """
+    Own class that inherits from int.
+    """
+    def __eq__(self, other):
+        """
+        Function that handles equality.
+        """
+        return int(self) == int(other)
+
+    def __ne__(self, other):
+        """
+        Function that handles inequality.
+        """
+        return not self.__eq__(other)
