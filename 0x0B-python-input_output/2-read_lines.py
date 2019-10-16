fc33b9c6320aab090e0795d05cc79ab9f2b93cc6
yenniferTobon <ymtobony@unal.edu.co>
2019-10-16 19:42:13 +0000

    2-read_lines.py  function that reads n lines of a text file (UTF8) and prints it to stdout:

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module for read file.
+"""
+number_of_lines = __import__('1-number_of_lines').number_of_lines
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    function that reads n lines of a text file (UTF8) and prints it to stdout:
+    """
+    totalLine = number_of_lines("my_file_0.txt")
+    if (nb_lines <= 0 or nb_lines >= totalLine):
+        nb_lines = totalLine
+    with open("my_file_0.txt", encoding="UTF-8") as f:
+        for numLine, line in enumerate(f):
+            if (numLine < nb_lines):
+                print(line, end="")
+            else:
+                break
