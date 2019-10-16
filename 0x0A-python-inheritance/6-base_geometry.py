af29d2e2b6e332eed158058514a3e9cab8c12364
yenniferTobon <ymtobony@unal.edu.co>
2019-10-16 02:41:04 +0000

    6-base_geometry.py def area(self): that raises an Exception with the message area() is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+BaseGeometry module
+"""
+class BaseGeometry():
+    """
+    Class that defines basic geometries.
+    """
+    def area(self):
+        """
+        Area of a geometry, not implemented by default.
+        """
+        raise Exception("area() is not implemented")   
