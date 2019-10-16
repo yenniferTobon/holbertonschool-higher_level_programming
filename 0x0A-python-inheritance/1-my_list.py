a49a88800677bfcdf99b5cab69dfb447c343e04b
yenniferTobon <ymtobony@unal.edu.co>
2019-10-16 02:37:07 +0000

    1-my_list.py prints the list, but sorted-ascending sort

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+MyList inherits from list
+Public instance method: print_stored() that prints the sorted list
+"""
+class MyList(list):
+    """
+    Class that inherits from list.
+    """
+    def print_sorted(self):
+        """
+        Function that prints a list sorted.
+        """
+        list_new = []
+        list_new = self.copy()
+        list_new.sort()
+        print(list_new)
