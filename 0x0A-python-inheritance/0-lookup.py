74ac7007bcdfa8ec1dc430e78eb04527a4faa782
yenniferTobon <ymtobony@unal.edu.co>
2019-10-16 03:10:26 +0000

    0-lookup.py  returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,8 +3,10 @@
 MyList inherits from list
 Public instance method: print_stored() that prints the sorted list
 """
+
+
 def lookup(obj):
     """
     Function that returns the list of attributes and methods of an object
     """
-    return dir(obj)    
+    return dir(obj)
