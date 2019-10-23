61b24e81f58a588d73b408b0a34ff69c9bb2ffd0
yenniferTobon <ymtobony@unal.edu.co>
2019-10-23 01:53:35 +0000

    square.py exercise 10 - 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,7 +34,7 @@ class Square(Rectangle):
     def __str__(self):
         """Return [Square] (<id>) <x>/<y> - <size>
         """
-        return "[{}] ({}) {}/{} - {}".format(
+        return '[{}] ({}) {}/{} - {}'.format(
                 self.__class__.__name__, self.id, self.x, self.y, self.width)
 
     def update(self, *argv, **kwargs):
