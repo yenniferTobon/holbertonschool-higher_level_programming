f881d38d4b332b859b99abce2d6594a367089c2e
yenniferTobon <ymtobony@unal.edu.co>
2019-11-27 22:32:59 -0500

    13-count_shows_by_genre.sql - script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT 	tv_genres.name as genre,
-	count(1) as number_of_shows
-FROM 	tv_genres, tv_show_genres
+SELECT  name as 'genre',
+        count(*) as 'number_of_shows'
+FROM    tv_genres, tv_show_genres
 WHERE   tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
+GROUP BY tv_show_genres.name
 ORDER BY  number_of_shows DESC;
