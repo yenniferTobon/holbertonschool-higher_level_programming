cd6e552061e9b46fb366f3b951a3129145dcc692
yenniferTobon <ymtobony@unal.edu.co>
2019-11-27 22:25:06 -0500

    10-genre_id_by_show.sql - script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres, tv_shows
-WHERE tv_shows.id = tv_show_genres.show_id
+SELECT 	tv_shows.title, tv_show_genres.genre_id
+FROM 	tv_show_genres, tv_shows
+WHERE 	tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
