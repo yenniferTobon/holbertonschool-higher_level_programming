749366f5df520da06fae60cb61fec0287455e9de
yenniferTobon <ymtobony@unal.edu.co>
2019-12-14 19:44:59 -0500

    2-my_filter_states.py displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-argv=sys.argv
-myConnection=MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3], port=3306, host='localhost')
-c=myConnection.cursor()
-c.execute("SELECT id,name FROM states WHERE name='{:s}' ORDER BY id ASC".format(argv[4]))
-for states in c.fetchall():
-	print(states)
-myConnection.close()
+"""list data from db according to a name passed"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    argv = sys.argv
+    myConnection = MySQLdb.connect(
+        user=argv[1],
+        password=argv[2], db=argv[3], port=3306, host='localhost')
+    c = myConnection.cursor()
+    c.execute(
+        "SELECT id, name FROM states WHERE name='{:s}'\
+        ORDER BY id ASC".format(argv[4]))
+    for states in c.fetchall():
+        print(states)
+    c.close()
+    myConnection.close()
