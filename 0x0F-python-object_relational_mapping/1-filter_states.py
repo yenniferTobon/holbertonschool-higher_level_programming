4f3b8f42a5dbd5429fb9dd2c798d482a3a32fab8
yenniferTobon <ymtobony@unal.edu.co>
2019-12-14 19:30:01 -0500

    1-filter_states.py script that lists all states with a name starting with N (upper N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-import MySQLdb
-myConnection= MySQLdb.connect(db='hbtn_0e_0_usa', host='localhost', port=3306, user='root', password='')
-c=myConnection.cursor()
-c.execute("SELECT id,name FROM states WHERE name like 'N%' ORDER BY id ASC")
-for states in c.fetchall():
-	print(states)
-myConnection.close()
+"""list data from db starting with 'N'"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+    argv = sys.argv
+    myConnection = MySQLdb.connect(
+        db=argv[3],
+        host='localhost', port=3306, user=argv[1], password=argv[2])
+    c = myConnection.cursor()
+    c.execute(
+        "SELECT id, name FROM states WHERE name like 'N%' ORDER BY id ASC")
+    for states in c.fetchall():
+        print(states)
+    myConnection.close()
