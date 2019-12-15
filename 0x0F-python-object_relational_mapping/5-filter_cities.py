e53d3fd82cb2ab5f543a978abb4bb35cd07b4b9a
yenniferTobon <ymtobony@unal.edu.co>
2019-12-14 23:47:19 -0500

    5-filter_cities.py takes in the name of a state as an argument and lists all cities of that state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""takes in the name of a state as an argument and lists all cities"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    argv = sys.argv
+    cities_name = ""
+    myConnection = MySQLdb.connect(
+        user=argv[1],
+        password=argv[2], db=argv[3], port=3306, host='localhost')
+    c = myConnection.cursor()
+    c.execute("SELECT cities.name\
+        FROM cities JOIN states\
+        ON cities.state_id = states.id\
+        WHERE states.name= BINARY %s\
+        ORDER BY cities.id ASC", (argv[4],))
+    for rows in c.fetchall():
+        cities_name = rows[0] + ", " + cities_name
+    cities_name = cities_name[:-2]
+    print("({})".format(cities_str))
+    c.close()
+    myConnection.close()
