666f107c101213be89e7ca57fa8165b51f3efd82
yenniferTobon <ymtobony@unal.edu.co>
2019-12-14 21:02:24 -0500

    3-my_safe_filter_states.py SQL injection-TRUNCATE TABLE states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""list data from db according to a name passed"""
+"""where name matches the argument -  SQL injection"""
 
 if __name__ == '__main__':
     import MySQLdb
@@ -10,9 +10,8 @@ if __name__ == '__main__':
         password=argv[2], db=argv[3], port=3306, host='localhost')
     c = myConnection.cursor()
     c.execute(
-        "SELECT id, name FROM states WHERE name LIKE BINARY '{:s}'\
-        ORDER BY id ASC".format(argv[4]))
+        "SELECT id, name FROM states WHERE name LIKE BINARY '%s'\
+        ORDER BY id ASC" % argv[4])
     for states in c.fetchall():
         print(states)
     c.close()
-    myConnection.close()
