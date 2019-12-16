7e27b88da4775d18ca9dc8cd7c64ceeb5893afa8
yenniferTobon <ymtobony@unal.edu.co>
2019-12-15 23:57:16 -0500

    model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
+
 class City(Base):
     """ City class that is similar a model State"""
     __tablename__ = 'cities'
