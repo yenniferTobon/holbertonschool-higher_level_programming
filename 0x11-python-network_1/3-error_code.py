e41b970fd804d7a3dc1ea5d92dc34e5ef42f3448
yenniferTobon <ymtobony@unal.edu.co>
2020-01-13 17:11:04 -0500

    3-error_code.py urllib.error.HTTPError exceptions and print: Error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""request to the URL- urllib.error.HTTPError exceptions and print: Error"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        response = urllib.request.urlopen(req)
+        print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as eRROR:
+        print("Error code: {}".format(eRROR.code))
