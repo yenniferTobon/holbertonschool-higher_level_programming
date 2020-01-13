8bb1c8836fe39c4361a0b453721eca8ac9afe685
yenniferTobon <ymtobony@unal.edu.co>
2020-01-13 14:02:44 -0500

    0-hbtn_status.py Write a Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,6 @@ import urllib.request
 with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
     response = res.read()
     print("Body response:")
-    print("\t- type: {}".format(type(response)))
-    print("\t- content: {}".format(response))
-    print("\t- utf8 content {}".format(response.decode('utf-8')))
+    print("\t-type: {}".format(type(response)))
+    print("\t-content: {}".format(response))
+    print("\t-utf8 content: {}".format(response.decode('utf-8')))
