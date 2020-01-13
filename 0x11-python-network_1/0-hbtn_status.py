b9839b8dea4b82f1ea24dc8748460161907cc9bb
yenniferTobon <ymtobony@unal.edu.co>
2020-01-13 13:56:48 -0500

    0-hbtn_status.py Write a Python script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
+    response = res.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
+    print("\t- utf8 content {}".format(response.decode('utf-8')))
