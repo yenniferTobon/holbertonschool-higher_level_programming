356039b4b15c42d4b134608238c546cd9b25df41
yenniferTobon <ymtobony@unal.edu.co>
2020-01-17 21:47:58 -0500

    10-my_github.py - script that takes your Github credentials (username and password) and uses the Github API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes your Github credentials and uses the Github API to display your id"""
+
+import requests as res
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        resp = res.get('https://api.github.com/user',  auth=(argv[1], argv[2]))
+        print(resp.json().get('id'))
+    except BaseException:
+        pass
