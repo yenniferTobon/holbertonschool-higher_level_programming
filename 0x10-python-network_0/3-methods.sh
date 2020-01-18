15c1fd3591de9d751297f9a03e283d48beb99dd1
yenniferTobon <ymtobony@unal.edu.co>
2020-01-18 17:38:50 -0500

    3-methods.sh  takes in a URL and displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept
-curl --head -s "$1" | grep "Allow" | cut -d " " -f 2,3,4
+curl -I -s "$1" | grep "Allow" | cut -d " " -f 2,3,4
