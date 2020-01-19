282bd2643e23801d191fd8333bb68a386434c8e9
yenniferTobon <ymtobony@unal.edu.co>
2020-01-18 22:41:59 -0500

    6-peak.py - function that finds a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,8 +4,11 @@ def find_peak(list):
     le = len(list)
     if le == 0:
         return
-    mayor = 0
-    for i in range(len(list)):
-        if (list[i] > mayor and i > 0):
-            mayor = list[i]
-    return mayor
+    de = le // 2
+    if (de == le - 1 or list[de] >= list[de + 1]) and\
+            (de == 0 or list[de] >= list[de - 1]):
+        return (list[de])
+    elif de != le - 1 and list[de + 1] > list[de]:
+        return (find_peak(list[de + 1:]))
+    else:
+        return (find_peak(list[:de]))
