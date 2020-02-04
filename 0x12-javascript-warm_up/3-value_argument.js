c8947df6d446ce4284f864f70fedc3ccdc000905
yenniferTobon <ymtobony@unal.edu.co>
2020-02-04 17:47:32 -0500

    3-value_argument.js prints the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 // prints the first argument passed to it
 
-process.argv.forEach((element, numVarible) => {
-  if (`${numVarible}` > 1) {
-    console.log(`${element}`);
-  }
-});
-
 if (process.argv[2] === undefined) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
