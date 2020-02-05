04b27968b9c116e1145835bca46a6429dfcd0161
yenniferTobon <ymtobony@unal.edu.co>
2020-02-04 22:58:51 -0500

    6-multi_languages_loop.js prints 3 lines but by using an array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints 3 lines but by using an array
+const all = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (const aux in all) {
+  console.log(all[aux]);
+}
