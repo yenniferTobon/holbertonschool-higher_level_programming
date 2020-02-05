ef896b7f63cb06380a1cad61d2d007b95ff96ac7
yenniferTobon <ymtobony@unal.edu.co>
2020-02-04 23:24:19 -0500

    7-multi_c.js prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,12 +3,10 @@
 
 const argument = parseInt(process.argv[2]);
 
-if (isNaN(argument)) {
+if ((isNaN(argument)) || (process.argv.length === 0)) {
   console.log('Missing number of occurrences');
 } else if (typeof argument === 'number') {
   for (let aux = 0; aux < argument; aux++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
