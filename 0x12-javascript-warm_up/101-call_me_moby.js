549dc5494f90b5249a0f02b8844b3419fd59c54c
yenniferTobon <ymtobony@unal.edu.co>
2020-02-05 23:17:08 -0500

    101-call_me_moby.js function that executes x times a function.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 exports.callMeMoby = function (x, theFunction) {
-    for (let num = 0; num <= x; num++) {
-      theFunction();
-    }
-  };
-  
+  for (let num = 0; num <= x; num++) {
+    theFunction();
+  }
+};
