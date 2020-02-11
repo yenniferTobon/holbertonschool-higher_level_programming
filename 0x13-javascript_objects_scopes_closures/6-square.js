cd8bdaa7450eb911d881a4f2203d5b86c098200c
yenniferTobon <ymtobony@unal.edu.co>
2020-02-10 22:41:01 -0500

    6-square.js instance method called charPrint(c)

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// instance method called charPrint(c)
+
+const SquareParent = require('./5-square');
+class Square extends SquareParent {
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let h = 1; h <= this.width; h++) {
+        for (let w = 1; w <= this.width; w++) {
+          process.stdout.write('C');
+        }
+        console.log();
+      }
+    }
+  }
+}
+module.exports = Square;
