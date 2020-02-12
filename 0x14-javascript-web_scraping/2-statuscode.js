d5de1c1e6f515edd5a58f4d9d5b3d6115af7bd7c
yenniferTobon <ymtobony@unal.edu.co>
2020-02-11 22:20:08 -0500

    2-statuscode.js display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// display the status code of a GET request
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
