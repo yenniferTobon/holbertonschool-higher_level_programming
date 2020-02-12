e802c7e33849423ebb82b1304778e9e0e536b120
yenniferTobon <ymtobony@unal.edu.co>
2020-02-11 22:01:44 -0500

    0-readme.js script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// script that reads and prints the content of a file.
+// Script that reads and prints the content of a file
 
-var fs = require('fs');
+const fs = require('fs');
+
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
-    return;
   } else {
     console.log(data);
   }
