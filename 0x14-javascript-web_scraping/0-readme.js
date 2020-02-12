c6d73def3adfaa2380fca5a45cbace97c033d0cd
yenniferTobon <ymtobony@unal.edu.co>
2020-02-11 21:31:06 -0500

    0-readme.js isplay the status code of a GET request

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+
+var fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
